f6bc223e06366218633ef80b83d90636b505ddff
Javier Benavides <>
2018-09-24 08:31:51 -0500

    refactor properties and add initial data for product review

---
+++ product-review-service/build/resources/main/data.sql
@@ -0,0 +1,15 @@
+/**
+ * Create Script for init of DB
+ */
+ -- Inser 6 product review.
+ --M20324, AC7836, C77154, BB5476, B42000
+insert into product_review (product_id, average_review_Score, number_of_reviews) values ('M20324', 10 , 100);
+
+insert into product_review (product_id, average_review_Score, number_of_reviews) values ('AC7836', 5, 500);
+
+insert into product_review (product_id, average_review_Score, number_of_reviews) values ('C77154', 6, 600);
+
+insert into product_review (product_id, average_review_Score, number_of_reviews) values ('BB5476', 7, 7000);
+
+insert into product_review (product_id, average_review_Score, number_of_reviews) values ('B42000', 7.8, 7800 );
+
